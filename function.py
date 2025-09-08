cdc71299fe9b1d8d5bb78b93aae386bf7cf2f272
Nicolas Rangel <81323849+angelzx1@users.noreply.github.com>
2025-09-08 11:46:46 -0300

    functions in python, each them solves the math operations

---
+++ function.py
@@ -0,0 +1,17 @@
+def summ(a, b):
+    return a + b
+
+def sub(c, d):
+    return c - d
+
+def mult(e, f):
+    return e * f
+
+def div(g, h):
+    return g/h
+
+#examples
+print("the value of the sum is " + str(summ(7,5)))
+print("the value of the subtraction is " + str(sub(10, 3)))
+print("the value of the multiplication is " + str(mult(8, 4)))
+print("the value of the division is " + str(div(10, 2)))
