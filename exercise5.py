46a6467df291496ee2d8f7d562db93fb16bcbe38
Nicolas Rangel <81323849+angelzx1@users.noreply.github.com>
2025-09-09 10:00:24 -0300

    exercise that a variable sums 2 variables type intengers in python.

---
+++ exercise5.py
@@ -0,0 +1,6 @@
+#Exercise that creates 2 ints variables and 1 variable that reuses 2 variables and sum them and after getting the result, then prints the result of the sum.
+
+number1 = 10
+number2 = 30
+calc = number1 + number2
+print(calc)
