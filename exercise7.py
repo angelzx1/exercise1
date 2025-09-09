4e15d3b233f3a0c4e43477de50bf0b6dcddafe79
Nicolas Rangel <81323849+angelzx1@users.noreply.github.com>
2025-09-09 14:09:15 -0300

    assigning the variables of others

---
+++ exercise7.py
@@ -0,0 +1,17 @@
+a = 30 # int
+b = True # boolean
+c = "thirdy" # str
+
+print(a) # prints 30
+print(b) # prints True
+print(c) # prints thirdy
+print() # prints a empty line
+
+a = c # a assigns c the value is thirdy
+b = a # b assigns a the value is thirdy
+c = b # c assigns b the value is thirdy
+
+#all print thirdy
+print(a)
+print(b)
+print(c)
