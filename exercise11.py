310676129d7624e375ab7c6bf73b38bb059d14ca
Nicolas Rangel <81323849+angelzx1@users.noreply.github.com>
2025-09-12 18:52:25 -0300

    exercise that you get input user the book's name and places on the stack

---
+++ exercise11.py
@@ -0,0 +1,11 @@
+#must be placed books in reverse order
+
+book1 = input("First book: ")
+book2 = input("Second book: ")
+book3 = input("Third book: ")
+
+
+print("Stack of books:")
+print(book3)
+print(book2)
+print(book1)
