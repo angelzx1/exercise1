9b02d8ca63e7b981404393ab4b1a4603c45dc362
Nicolas Rangel <81323849+angelzx1@users.noreply.github.com>
2025-09-14 11:06:23 -0300

    the attention about function

---
+++ exercise12.py
@@ -0,0 +1,4 @@
+print("Enter a number: ")
+user_num = input()
+# user enters 10
+print(user_num + user_num) # will output 1010, because the function in variable "user_num" haven't implemented int() to convert string in intenger, the number will be string like "10"
