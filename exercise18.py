059d7062d4c14d5620bc27f652f07191455ccb83
Nicolas Rangel <81323849+angelzx1@users.noreply.github.com>
2025-09-25 20:59:32 -0300

    calculator with sum and difference

---
+++ exercise18.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum_of_numbers = num1 + num2
+difference_of_numbers = num1 - num2
+print("The sum of your numbers is: " + str(sum_of_numbers))
+print("The difference of your numbers is: " + str(difference_of_numbers))
